a10c532519cc62a06debbffb6b553125cc6328ee
rogerxaic <roger.miret@gmail.com>
2021-03-19 18:18:38 +0000

    fix freezer port

--- api/freezerURL.py
+++ api/freezerURL.py
@@ -7,4 +7,4 @@ url_blueprint = Blueprint("freezerurl", __name__)
 @url_blueprint.route("/freezerurl")
 @swag_from('freezerURL.yaml')
 def getFreezerURL():
-    return jsonify({"freezerMgrURL": os.getenv('FREEZER_MGR_URL','http://localhost:8082')})
+    return jsonify({"freezerMgrURL": os.getenv('FREEZER_MGR_URL','http://localhost:8080')})
